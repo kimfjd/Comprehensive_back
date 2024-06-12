c9a97e6ea105dfd281c4a897c178163be786b02d
kimfjd <kimfjd@naver.com>
2024-06-12 12:26:30 +0900

    초기

---
+++ src/main/java/com/sick/apeuda/ApeudaApplication.java
@@ -0,0 +1,13 @@
+package com.sick.apeuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApeudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApeudaApplication.class, args);
+	}
+
+}
